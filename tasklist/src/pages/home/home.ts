a305e257664c4c11a694c4fdb81d19d476d699a2
Alejandro Cremades <residence@studiokura.com>
2020-05-14 20:36:51 +0900

    change form

--- tasklist/src/pages/home/home.ts
+++ tasklist/src/pages/home/home.ts
@@ -6,7 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+title: string='task登録'
+tasks:{name: string}[]=[
+  {name:'task1'},
+   {name:'task2'},
+];
   constructor(public navCtrl: NavController) {
 
   }
