d99d8480a82032f3433c72f498f4d8edb2da971d
Alejandro Cremades <residence@studiokura.com>
2020-06-11 20:14:56 +0900

    save

--- tasklist/src/pages/task-list/task-list.ts
+++ tasklist/src/pages/task-list/task-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams ,ActionSheetController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the TaskListPage page.
@@ -8,25 +8,76 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-task-list',
-  templateUrl: 'task-list.html',
-})
-export class TaskListPage {
-tasks: {name: string}[] = [
-  {name:'タスク１'},
-  {name:"タスク２"},
-];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-ionViewWillEnter(){
-  if(localStorage.getItem('tasks')){
-    this.tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-}
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TaskListPage');
-  }
+ @IonicPage()
+ @Component({
+   selector: 'page-task-list',
+   templateUrl: 'task-list.html',
+ })
+ export class TaskListPage {
+ tasks: {name: string}[] = [
+   {name:'タスク１'},
+   {name:"タスク２"},
+ ];
+   constructor(public navCtrl: NavController,public navParams: NavParams,public actionSheetCtrl: ActionSheetController,public alertCtrl: AlertController){}
+ ionViewWillEnter(){
+   if(localStorage.getItem('tasks')){
+     this.tasks = JSON.parse(localStorage.getItem('tasks'));
+   }
+ }
+   ionViewDidLoad() {
+     console.log('ionViewDidLoad TaskListPage');
+   }
 
+changeTask(index: number){
+  let actionSheet= this.actionSheetCtrl.create({
+    title:'タスクの変更',
+    buttons:[
+      {
+        text:'削除',
+        role:'destructive',
+        handler:() =>{
+        this.tasks.splice(index, 1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+        }
+      },{
+        text:'変更',
+        handler: () => {
+          this._renameTask(index);
+        }
+      },{
+      text: '閉じる',
+      role:'cancel',
+      handler: () => {
+        console.log('cancel clicked');
+      }
+      }
+    ]
+  });
+actionSheet.present();
+}
+_renameTask(index: number){
+  let prompt = this.alertCtrl.create({
+    title: '変更後のタスク',
+    inputs: [
+      {
+        name: 'task',
+        placeholder: 'タスク',
+        value: this.tasks[index].name
+      },
+    ],
+    buttons:[
+      {
+        text:'閉じる'
+      },
+      {
+        text: '保存',
+        handler: data => {
+          this.tasks[index] = {name:data.task};
+          localStorage.setItem('tasks',JSON.stringify(this.tasks));
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
 }
